240fdcf1f450b9076cdb934a220bf119d792ec12
Neil Gardner <neilgardner1963@gmail.com>
2018-11-29 20:09:33 +0000

    End of section 3

--- src/app/layouts/App.jsx
+++ src/app/layouts/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+import Grid from '../../features/event/EventDashboard/EventDashboard'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
